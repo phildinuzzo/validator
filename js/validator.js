42a4e31cbf34c25911df7bb4940d790a04e8eb42
Phil DiNuzzo <phil248@gmail.com>
2013-05-02 11:31:34 -0700

    Name and phone validator implemented.

--- js/validator.js
+++ js/validator.js
@@ -1,12 +1,50 @@
 $(document).ready(function(){
 
+  $('#name_input').on('blur', function () {
+    var name = $('#name_input').val();
+    if ((name.trim() === ('')) || (name.length < 3) || !(isNaN(name)))
+      {
+        $("#name_error").removeClass('hidden');
+      } else {
+        $('#name_error').addClass('hidden');
+        $('#name_display').append(name);
+      }
+  }); // end function
+
+  $('#age_input').on('blur', function () {
+    var age = $('#age_input').val();
+    if ((age.trim() === ('')) || (parseInt(age) < 1 ) || (parseInt(age) > 110) || (isNaN(age)))
+      {
+        $("#age_error").removeClass('hidden');
+      } else {
+        $('#age_error').addClass('hidden');
+        $('#age_display').append(name);
+      }
+  }); // end function
+
+  $('#phone_input').on('blur', function () {
+    var phone = $('#phone_input').val();
+    if ((phone.trim() === ('')))
+      {
+        $("#phone_error").removeClass('hidden');
+      } else {
+        $('#phone_error').addClass('hidden');
+        $('#phone_display').append(name);
+      }
+  }); // end function
 
 
 
-  var name = $('#name_input').val();
-  console.log(name);
 
 
+// var 1 = 5;
+// if(i > 7) {
+//   console.log("hello");
+// } else if(i < 3) {
+//   console.log("bye");
+// } else {
+//   consol.log("Ok");
+// }
 
 
 
