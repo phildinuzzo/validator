61e688b51a9a4be5c1eede27083931efb045358c
Phil DiNuzzo <phil248@gmail.com>
2013-05-02 12:05:13 -0700

    validators added

--- js/validator.js
+++ js/validator.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   $('#name_input').on('blur', function () {
     var name = $('#name_input').val();
-    if ((name.trim() === ('')) || (name.length < 3) || !(isNaN(name)))
+    if ((name.trim() === ('')) || (name.trim().length < 3) || !(isNaN(name)))
       {
         $("#name_error").removeClass('hidden');
       } else {
@@ -18,21 +18,32 @@ $(document).ready(function(){
         $("#age_error").removeClass('hidden');
       } else {
         $('#age_error').addClass('hidden');
-        $('#age_display').append(name);
+        $('#age_display').append(age);
       }
   }); // end function
 
   $('#phone_input').on('blur', function () {
     var phone = $('#phone_input').val();
-    if ((phone.trim() === ('')))
+    if ((phone.trim() === ('')) || (phone.length != 10) || (isNaN(phone)))
       {
         $("#phone_error").removeClass('hidden');
       } else {
         $('#phone_error').addClass('hidden');
-        $('#phone_display').append(name);
+        $('#phone_display').append(phone);
       }
   }); // end function
 
+
+  $('#email_input').on('blur', function () {
+    var email = $('#email_input').val();
+    if ((email.trim() === ('')))
+      {
+        $("#email_error").removeClass('hidden');
+      } else {
+        $('#email_error').addClass('hidden');
+        $('#email_display').append(email);
+      }
+  }); // end function
 
 
 
